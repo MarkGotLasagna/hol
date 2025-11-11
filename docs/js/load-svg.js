f4dc2347e40843f47b81684646f3361b5c8d491a
Marco Rondelli <marcorondelli1@gmail.com>
2025-11-11 13:17:26 +0100

    Changed references to files

--- docs/js/load-svg.js
+++ docs/js/load-svg.js
@@ -17,5 +17,5 @@ fetch('Timeline.svg')
         console.error('Error loading SVG:', error);
         // Fallback to img tag if loading fails
         const container = document.getElementById('svg-container');
-        container.innerHTML = '<img src="Timeline.svg" alt="History of Linux Timeline">';
+        container.innerHTML = '<img src="../resources/Timeline.svg" alt="History of Linux Timeline">';
     });
